14380c12479ec777813bb5de6c26fbdf0cd844d4
Ankush Kanekar <90831148+Ankush922@users.noreply.github.com>
2022-05-09 13:19:35 +0530

    Initial commit

---
+++ Spring Lab 6/src/main/java/com/College_Test/studentManagement/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.College_Test.studentManagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.greatLearning.studentManagement.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
